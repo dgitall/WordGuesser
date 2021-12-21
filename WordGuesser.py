527456ceb371b4ff8dd4bea5952e7793414e58a7
Darrell Gerber <darrellgerber@gmail.com>
2021-12-21 16:19:05 -0600

    Step 4

--- WordGuesser.py
+++ WordGuesser.py
@@ -1,48 +1,60 @@
 import random
+from urllib.request import urlopen
 
+## Open a file from a github repository containing a bunch of words
+## Load this into our internal dictionary
 def OpenDictionary():
-    dictionary = {
-        0: 'creamer',
-        1: 'apples',
-        2: 'pears',
-        3: 'cough',
-        4: 'salmon'
-    }
-    
+    dictionary = dict()
+    print("Loading dictionary...")
+   
+    with urlopen("https://github.com/dwyl/english-words/blob/master/words_alpha.txt?raw=true") as data:
+        # Transform the bitstream into strings
+        f = data.read()
+        texts = f.decode(encoding='utf-8', errors='ignore')
+        # Split up the text into a list of words
+        lines = texts.split('\r\n')
+        counter=0
+        # Load the words into the dictionary
+        for word in lines:
+            dictionary[counter] = word
+            counter+=1
+  
     return dictionary
 
+## Randomly pick a word from our dictionary for the user to guess
 def SelectWord(dictionary):
     # Remove an arbitrary selection from the dictionary
     # Removing will ensure the word is not selected again
     randomKey = random.randint(0, len(dictionary))
     SelectedWord = str(dictionary.pop(randomKey))
-    
     SelectedWord.lower()
+    
     return SelectedWord
 
+## Initialize the displaylist object which says which character of the word to display in the clue
 def InitDisplayList(Word):
     DisplayList = list()
-    
     for char in Word:
         DisplayList.append(False)
     
     return DisplayList
 
+## Display the clue with '_' in places where they haven't already guessed the character
+## This is stored in the DisplayList as true where the word characters are shown
 def ShowWord(Word, DisplayList):
-    # Create the Displayed version of the word with '_' whereever
-    # DisplayList is False and the character from Word where
-    # it is true
-    DisplayWord = str()
+    # Create the Displayed version of the word starting with the Word characters
+    # Change to add '_' where DisplayList is False 
+    DisplayWord = list(Word)
     for index, char in enumerate(Word):
-        if DisplayList[index]:
-            DisplayWord += char
-        else:
-            DisplayWord += "_"
-    
-    # Show DisplayWord as a Clue
-    print("Clue: %s (%d letters)" % (DisplayWord, len(DisplayWord)))
-    
+        if not DisplayList[index]:
+            DisplayWord[index] = "_"
 
+    joined_string = " ".join(DisplayWord)
+    # Show DisplayWord as a Clue
+    print("Clue: %s (%d letters remaining)" %
+          (joined_string, len(DisplayList)-sum(DisplayList)))
+    
+## Check to see if UserInput matches any letters in Word. Where it does, se DisplayList to True
 def CheckChar(Word, DisplayList, UserInput):
     # Go through all of the characters in the Word and turn on the display
     # for each that matches the User Input
@@ -52,7 +64,7 @@ def CheckChar(Word, DisplayList, UserInput):
 
 
         
-
+## Prompt the user for input and verify that it is a string and not empty.
 def GetUserInput():
     UserInput = str()
     GetInput = True
@@ -79,37 +91,54 @@ def GetUserInput():
 # Main Body
 dictionary = OpenDictionary()
 
+# Loop through playing the game until the user says to stop
 PlayingGame = True
+GameCount = 0
 while PlayingGame:
+    # Check to make sure that we haven't used up all of the words in our dictionary
+    if len(dictionary) == 0:
+        break
+    # Select the word for the play to guess
     Word = SelectWord(dictionary)
 
     DisplayList = InitDisplayList(Word)
 
     TurnCounter = 0
+    WinCount = 0
+    GameCount += 1
     GameMessage = "Sorry....Game Over. Better luck next time."
     SuccessMessage = "Congratulations!!!! You have successfully guessed the word"
+    ## Give the player 7 tries to guess the word. 
     while TurnCounter < 7:
+        ## Display the clue
+        print("Round: %s" % (TurnCounter + 1))
         ShowWord(Word, DisplayList)
-
+        # Get the users guess
         UserInput = GetUserInput()
-
+        
+        # If the user enters a single character check where it is in the word
         if(len(UserInput) == 1):
             # Check if character is in the word list
             CheckChar(Word, DisplayList, UserInput)
-            # Need to check if all characters filled in the
+            # Check if all characters filled in the word
             if(sum(DisplayList) == len(DisplayList)):
                 GameMessage = SuccessMessage
+                WinCount += 1
                 break
             TurnCounter += 1
+        # If the user entered multiple characters this is a word guess 
         else:
             # Check if the guess is correct
             if UserInput == Word:
                 GameMessage = SuccessMessage
+                WinCount += 1
                 break
             
-    print(GameMessage + "(%s rounds)" % (TurnCounter))
+    print(GameMessage + " The word was: %s (%s/%s correct words)" % 
+          (Word, sum(DisplayList), len(DisplayList)))
     PlayAgain = input("Would you like to play again? (y/n)")
     if(PlayAgain != 'y'):
+        print("You won %s out of %s games" % (WinCount, GameCount))
         print("Goodbye")
         PlayingGame = False
             
